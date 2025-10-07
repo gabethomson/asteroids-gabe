aefae720304777ac7ac5c0d16a46ae533462f729
gabethomson <galle.gabriel@hotmail.com>
2025-10-07 16:20:57 +0200

    A: Init Pygame, Constants set

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids Game")
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+    
+
+if __name__ == "__main__":
+    main()
